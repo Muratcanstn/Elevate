48ae4c4aa3b677723db1253d9822e57b39aecb4c
Mourate <muratcan03.stn@gmail.com>
2022-10-29 17:16:59 +0200

    wip

--- share_button.js
+++ share_button.js
@@ -1,6 +1,8 @@
 const shareBtn = document.querySelector('.share-btn');
 const shareOptions = document.querySelector('.share-options');
 
+console.log(shareBtn)
 shareBtn.addEventListener('click', () => {
     shareOptions.classList.toggle('active');
+    console.log('hello')
 })
